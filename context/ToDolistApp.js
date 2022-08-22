0931aeda76c9bdf6347511971bedfff2063556dd
Anurag Mishra <2000520100019@ietlucknow.ac.in>
2022-08-22 17:03:02 +0530

    Update ToDolistApp.js

--- context/ToDolistApp.js
+++ context/ToDolistApp.js
@@ -1,1 +1,48 @@
+import React, {useState} from "react";
+import Web3Modal from 'web3modal';
+import {ethers} from 'ethers';
 
+
+//  import files form other folders (internal import)
+
+import {toDolistAddress, toDolistABI} from "./constants";
+
+const  fetchContract = (signerOrProvider) =>
+new ethers.Contract(toDolistAddress, toDolistABI, signerOrProvider);
+
+export const ToDolistContext= React.createContext();
+export const ToDolistProvider= ({children})=>{
+                // some use-state variables \\
+
+    const [currentAccount, setcurrentAccount] = useState('');
+    const [error, seterror] = useState('');
+    const [allToDolist, setallToDolist] = useState([]);
+    const [mylist, setmylist] = useState([]);
+
+    const [AllAddress, setAllAddress] = useState([]);
+  
+/*
+
+  ********************************Connecting MetaMask**********************************
+
+ */
+
+  const checkifwalletisconnected = async() =>{
+    
+  }
+
+    return(
+        <ToDolistContext.Provider value={{}}>
+            {children}
+        </ToDolistContext.Provider>
+    );
+};
+const ToDolistApp=() =>{
+    return (
+        <div>
+            TODOLISTAPP
+        </div>
+
+    );
+}
+export default ToDolistApp
--- context/constants.js
+++ context/constants.js
@@ -1,5 +1,13 @@
 // contract address: 0x5FbDB2315678afecb367f032d93F642f64180aa3
 
-import ToDolist from './ToDolist.json'
 
+// We've to access the abi from the json file. for that first we import the file having ABI 
+
+import ToDolist from "./ToDolist.json"
+
+// now we'll export the address and the abi
+
+export const toDolistAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const toDolistABI = toDolist.abi;
+// now as we exported the ABI, we've all the access of the functions and data inside the abi
 
